ac303884b6280788b83fc17aa2b4b9eca4c0de5b
dilaksh06 <kamalathasandilakshan@gmail.com>
2025-10-01 01:53:43 +0530

    log in screen minor ui.ux imporved

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native'
 import React, { useState } from 'react'
-
+import LoginScreen from './src/screens/LoginScreen'
 export default function App() {
 
-  const[email,setEmail]=useState("message")
-  return (
-    <View style={{margin:4,backgroundColor:"white",flex:1}}>
-      <View style={{flex:2,width:"55%"}}>
-        <Text>App</Text>
+  
 
-      <TextInput 
-      style={{borderColor:"#2ec770",borderWidth:1,borderRadius:5}}
-      placeholder='email'
-      />
-      </View>
-    </View>
+  return (
+  <LoginScreen/>
   )
 }
+
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native'
+import React, { useState } from 'react'
 
 const LoginScreen = () => {
+  
+const [email, setEmail] = useState<string>("")
+const [paswd, setPaswd] = useState<string>("")
+const [lists, setLists] = useState<string[]>([])
+
+function collectData(text: string) {
+  setLists([...lists, text])  // ✅ appends the string to the list
+}
+
+    function submit(){
+      Alert.alert("hi you have pressed the button")
+    }
+
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+      <View style={styles.mainView}>
+      <View style={{ flex: 2, width: "55%" }}>
+        <Text>Email</Text>
+
+        <TextInput
+          style={styles.inputBox}
+          placeholder='email'
+          onChangeText={setEmail}
+        />
+
+        <Text>password</Text>
+
+        <TextInput
+          style={styles.inputBox}
+          placeholder='email'
+        />
+        <Button
+        title='submit'
+        onPress={submit}/>
+      </View>
     </View>
   )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create(
+
+  {
+    mainView: {
+      flex: 1,
+      backgroundColor: "white",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    inputBox: {
+      borderColor: "#2ec770",
+      borderWidth: 1,
+      borderRadius: 5,
+      color:"black"
+      
+    }
+
+
+  }
+)
